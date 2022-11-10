b12b38abe1bdaf82b38f00e6149c5884f6c624d3
Ethan Talbert <ethangtalbert@gmail.com>
2022-11-10 13:42:12 -0800

    Add grading script

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,74 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
+class LongerThan2 implements StringChecker {
+  public boolean checkString(String s) {
+    if (s.length() > 2) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class isHello implements StringChecker {
+  public boolean checkString(String s) {
+    if (s.equals("Hello")) {
+      return true;
+    }
+    return false;
+  }
+}
+
 public class TestListExamples {
-  // Write your grading tests here!
+	@Test 
+	public void testGeneralFilter() {
+    List<String> input1 = new ArrayList<String>();
+    input1.add("Hello"); input1.add("Hell"); input1.add("He");
+    List<String> expected = new ArrayList<String>();
+    expected.add("Hello"); expected.add("Hell");
+
+    StringChecker sc = new LongerThan2();
+    input1 = ListExamples.filter(input1, sc);
+    assertEquals(expected, input1);
+	}
+
+  @Test 
+	public void testEmptyFilter() {
+    List<String> input1 = new ArrayList<String>();
+    input1.add("Hell"); input1.add("He");
+    List<String> expected = new ArrayList<String>();
+
+    StringChecker sc = new isHello();
+    input1 = ListExamples.filter(input1, sc);
+    assertEquals(expected, input1);
+	}
+
+  @Test 
+	public void testGeneralMerge() {
+    List<String> input1 = new ArrayList<String>();
+    input1.add("a"); input1.add("b"); input1.add("d");
+    List<String> input2 = new ArrayList<String>();
+    input2.add("c"); input2.add("e"); input2.add("f");
+    List<String> expected = new ArrayList<String>();
+    expected.add("a"); expected.add("b"); expected.add("c");
+    expected.add("d"); expected.add("e"); expected.add("f");
+
+    List<String> result = ListExamples.merge(input1, input2);
+    assertEquals(expected, result);
+	}
+
+  @Test 
+	public void testEmptyFirstMerge() {
+    List<String> input1 = new ArrayList<String>();
+    List<String> input2 = new ArrayList<String>();
+    input2.add("d"); input2.add("e"); input2.add("f");
+    List<String> expected = new ArrayList<String>();
+    expected.add("d"); expected.add("e"); expected.add("f");
+
+    List<String> result = ListExamples.merge(input1, input2);
+    assertEquals(expected, result);
+	}
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,43 @@
 # Create your grading script here
 
-set -e
+CP=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 
+echo "Getting Submission"
 rm -rf student-submission
-git clone $1 student-submission
+git clone $1 student-submission 2> /dev/null
+cp TestListExamples.java student-submission/
+cd student-submission/
 
+# Checking if the submission has the file we are using. If not, exit and give an error message
+if [ -f "ListExamples.java" ]; then
+	echo "Compiling Files"
+else
+	echo "WRONG FILENAME! Your submission should have a file named ListExamples.java"
+	exit
+fi
+
+javac -cp $CP *.java 2> compiler_output.txt
+
+# Checking if there are any compiling errors
+if [ $? -eq "0" ]; then
+       echo "Code Compiled"	
+else
+	echo "ERROR COMPILING! See error message below for more details"
+	cat compiler_output.txt
+	exit
+fi
+
+echo "Starting Tests"
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > test_output.txt
+echo "Finished Tests"
+
+# TNUM is the number of total tests, and ENUM is the number of failed tests
+awk -F\. '{print NF-1}' test_output.txt > temp.txt
+TNUM=$(sed -e '1d;3,$d' temp.txt)
+awk -F\E '{print NF-1}' test_output.txt > temp.txt
+ENUM=$(sed -e '1d;3,$d' temp.txt)
+
+# PNUM takes the number of passed tests
+PNUM=$(($TNUM - $ENUM))
+
+echo "You have passed $PNUM out of $TNUM tests, giving you a score of $PNUM/$TNUM"
